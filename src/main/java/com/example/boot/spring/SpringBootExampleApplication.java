4e41241a9274286805c50b8e47e16501a6a4ff90
jwsmith2340 <92739468+jwsmith2340@users.noreply.github.com>
2022-06-21 11:17:09 -0400

    In the process of bringing in Postgres to learn models.

--- src/main/java/com/example/boot/spring/SpringBootExampleApplication.java
+++ src/main/java/com/example/boot/spring/SpringBootExampleApplication.java
@@ -3,6 +3,7 @@ package com.example.boot.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableAutoConfiguration
 public class SpringBootExampleApplication {
 
-	@RequestMapping("/")
+	@GetMapping("/")
 	String home() {
 		return "Hello Dumbass!";
 	}
